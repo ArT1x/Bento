2491431ba1fad012e8173bda607d6ac6be4d587b
ArT1x <artix@gmx.net>
2020-12-24 15:21:28 +0100

    Zum Commit vorgemerkte Änderungen: 	geändert:       js/greeting.js namen in greeting.js geändert

--- js/greeting.js
+++ js/greeting.js
@@ -3,18 +3,18 @@ var today = new Date();
 var hour = today.getHours();
 
 // Here you can change your name
-var name = 'John Doe';
+var name = 'emilio';
 
 // Here you can change your greetings
-var gree1 = 'Go to Sleep! ';
-var gree2 = 'Good morning! ';
-var gree3 = 'Good afternoon ';
-var gree4 = 'Good evening ';
-var gree5 = 'Good evening ';
-var gree6 = 'Good evening ';
+var gree1 = 'Geh schlafen :) ';
+var gree2 = 'Guten Morgen ;
+var gree3 = 'Guten Mittag ';
+var gree4 = 'Guten Abend ';
+var gree5 = 'Guten Abend ';
+var gree6 = 'Guten Abend ';
 
 // Define the hours of the greetings
-if (hour >= 23 && hour < 5) {
+if (hour >= 23 && hour < 1) {
     document.getElementById('greetings').innerText = gree1 + name;
 } else if (hour >= 6 && hour < 12) {
     document.getElementById('greetings').innerText = gree2 + name;
